fde785ab9e6d08474c69e88c2d19329c745edafe
AfnanSD <99503463+AfnanSD@users.noreply.github.com>
2022-12-13 10:11:50 +0300

    speech to text
    https://www.youtube.com/watch?v=dpdnvSI0Dn8

--- tamakan/lib/main.dart
+++ tamakan/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 import 'package:audioplayers/audioplayers.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,28 +37,87 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final player = AudioPlayer();
+  late stt.SpeechToText speech;
+  bool isListening = false;
+  String text = 'press';
+  double confidence = 1;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    speech = stt.SpeechToText();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            // Create a storage reference from our app
-            final storageRef = FirebaseStorage.instance.ref();
+        body: Column(
+          children: [
+            Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  // Create a storage reference from our app
+                  final storageRef = FirebaseStorage.instance.ref();
 
-            // Create a reference with an initial file path and name
-            final pathReference = storageRef.child("/practices/ألف.mp3");
-            // Create a reference to a file from a Google Cloud Storage URI
-            final gsReference = FirebaseStorage.instance
-                .refFromURL("gs://tamakan-ef69b.appspot.com/practices/ألف.mp3");
+                  // Create a reference with an initial file path and name
+                  final pathReference = storageRef.child("/practices/ألف.mp3");
+                  // Create a reference to a file from a Google Cloud Storage URI
+                  final gsReference = FirebaseStorage.instance.refFromURL(
+                      "gs://tamakan-ef69b.appspot.com/practices/ألف.mp3");
 
-            await player
-                .play(DeviceFileSource(await gsReference.getDownloadURL()));
-            print('5');
-          },
-          child: Text('play'),
+                  await player.play(
+                      DeviceFileSource(await gsReference.getDownloadURL()));
+                  print('5');
+                },
+                child: Text('play'),
+              ),
+            ),
+            SingleChildScrollView(
+              reverse: true,
+              child: Container(
+                child: Text(text),
+              ),
+            )
+          ],
         ),
-      ),
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: AvatarGlow(
+          animate: isListening,
+          glowColor: Theme.of(context).primaryColor,
+          endRadius: 75.0,
+          duration: const Duration(milliseconds: 2000),
+          repeatPauseDuration: const Duration(milliseconds: 100),
+          repeat: true,
+          child: FloatingActionButton(
+            onPressed: listen,
+            child: Icon(isListening ? Icons.mic : Icons.mic_none),
+          ),
+        ));
+  }
+
+  void listen() async {
+    print('listen');
+    if (!isListening) {
+      bool available = await speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => isListening = true);
+        speech.listen(
+          onResult: (val) => setState(() {
+            print(val.recognizedWords + '***');
+            text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => isListening = false);
+      speech.stop();
+    }
   }
 }
