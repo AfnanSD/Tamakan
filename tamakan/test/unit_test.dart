5f34fc06fd657c8d61789426ad3a3aee657d9bb9
RubaAbdullah <100301899+RubaAbdullah@users.noreply.github.com>
2023-02-15 01:50:10 +0300

    .

--- tamakan/test/unit_test.dart
+++ tamakan/test/unit_test.dart
@@ -1,17 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 class MockFirebaseAuth extends Mock implements FirebaseAuth {}
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   group('Firebase Authentication Tests', () {
-   late MockFirebaseAuth mockFirebaseAuth;
+    late MockFirebaseAuth mockFirebaseAuth;
     // Setup the mock authentication instance before each test
     setUp(() {
       Firebase.initializeApp();
-       mockFirebaseAuth = MockFirebaseAuth();
+      mockFirebaseAuth = MockFirebaseAuth();
     });
 
     test('Sign in with email and password', () async {
@@ -19,7 +21,7 @@ void main() {
       final mockUser = MockUser();
       // Set the user to be returned by the Firebase API when sign-in is called
       when(mockFirebaseAuth.signInWithEmailAndPassword(
-          email: 'test@example.com', password: 'password'))
+              email: 'test@example.com', password: 'password'))
           .thenAnswer((_) => Future.error(mockUser));
 
       // Call the sign-in method with the email and password
